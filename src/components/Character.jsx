363f6fb4942915c7cddec350285f466fb5efc0d8
Raul Tierno <raultm@gmail.com>
2024-05-07 12:38:26 +0200

    Character - Rick and Morty API

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -6,7 +6,7 @@ export const Character = () => {
 
     return (
         <>
-            <img src="https://m.media-amazon.com/images/I/61V11mSkh8L._AC_SL1500_.jpg" height="300px" alt="Imagen"></img>
+            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" height="300px" alt="Imagen"></img>
             <h1>{nombre}</h1>
             <p>{especie}</p>
         </>
