50dac2b303d2cbacde08edf1d48c8582e5216411
Raul Tierno <raultm@gmail.com>
2024-05-08 09:47:53 +0200

    CharList - Rick y Morty

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -2,22 +2,24 @@
 export const nombre = "Rick Sánchez"
 export const especie = "Humano"
 
-export const Character = () => {
+export const Character = (data) => {
+
+    console.log(data)
 
     return (
         <>
             <div className="relative w-64 p-4 m-auto bg-white shadow-lg rounded-2xl dark:bg-gray-900">
                 <div className="w-full h-full text-center">
                     <div className="flex flex-col justify-between h-full">
-                        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Imagen"></img>
+                        <img src={data.src} alt="Imagen"></img>
                         <p className="absolute text-sm italic text-gray-800 dark:text-white top-2 right-2">
                             
                         </p>
                         <p className="mt-4 text-lg text-gray-900 dark:text-white">
-                            {nombre}
+                            {data.nombre}
                         </p>
                         <p className="px-6 py-2 text-xs font-thin text-gray-700 dark:text-gray-50">
-                            {especie}
+                            {data.especie}
                         </p>
                     </div>
                 </div>
--- src/components/CharactersList.jsx
+++ src/components/CharactersList.jsx
@@ -1,20 +1,13 @@
 import { Character } from "./Character"
+import { character, characters } from "../utils/data"
+
 
 export const CharactersList = () => {
     return (
         <>
             <h1 className="text-4xl font-bold p-10 justify-center">Galería de Rick y Morty</h1>
             <div className="grid grid-cols-4 gap-4 justify-center">
-                <Character />
-                <Character />
-                <Character />
-                <Character />
-                <Character />
-                <Character />
-                <Character />
-                <Character />
-                <Character />
-                <Character />
+                {characters.results.map(character => <Character nombre={character.name} especie={character.species} src={character.image}/>)}
             </div>
         </>
     )
