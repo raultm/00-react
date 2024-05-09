09c6089a96f4238920ba0e68ecee4e22c39f59b0
Raul Tierno <raultm@gmail.com>
2024-05-09 10:13:44 +0200

    Hook - Refactor to custom hook

--- src/components/CharactersList.jsx
+++ src/components/CharactersList.jsx
@@ -1,30 +1,18 @@
 import { Character } from "./Character"
-import { api, character, characters } from "../utils/data"
-import { useEffect, useState } from "react"
+import { useApiList } from "../hooks/useApiList"
 
 
 export const CharactersList = () => {
 
-    const [charactersList, setCharactersList] = useState(characters);
-    const [page, setPage] = useState(1);
-
-    useEffect(() => { fetchCharacters() }, [page])
-
-    const fetchCharacters = async () => { setCharactersList( await api.characters(page) ) }
-
-    const handleSiguienteClick = () => { setPage(prevPage => prevPage + 1) }
-
-    const handleAnteriorClick  = () => { setPage(prevPage => prevPage - 1) }
+    const {charactersList, page, handleAnteriorClick, handleSiguienteClick} = useApiList([])
     
-
     return (
         <>
             <h1 className="text-4xl font-bold p-10 justify-center">Galería de Rick y Morty</h1>
             {(page>1) && <button onClick={handleAnteriorClick}>Anterior</button>}
             Pagina {page} - <button onClick={handleSiguienteClick}>Siguiente</button>
             <div className="grid grid-cols-4 gap-4 justify-center">
-                {charactersList.results
-                    //.filter(character => character.status == "Alive")
+                { charactersList
                     .map(character => <Character key={character.id} showIcon={character.id % 3 == 0} nombre={character.name} especie={character.species} src={character.image} />)}
             </div>
         </>
---
+++ src/hooks/useApiList.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+import { api } from "../utils/data"
+
+export const  useApiList= (list) => {
+  
+    const [charactersList, setCharactersList] = useState(list);
+    const [page, setPage] = useState(1);
+
+    useEffect(() => { fetchCharacters() }, [page])
+
+    useEffect(() => { console.log(charactersList) }, [charactersList])
+
+    const fetchCharacters = async () => { setCharactersList( (await api.characters(page)).results ) }
+
+    const handleSiguienteClick = () => { setPage(prevPage => prevPage + 1) }
+
+    const handleAnteriorClick  = () => { setPage(prevPage => prevPage - 1) }
+  
+    return {
+        charactersList,
+        page,
+        handleSiguienteClick,
+        handleAnteriorClick
+    }
+}
